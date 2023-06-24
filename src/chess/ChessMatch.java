fef00796c50b6c67118de652452f2391ea016ec1
LopezTw <caiohg_94@hotmail.com>
2023-06-24 16:33:07 -0300

    Primeiro Commit do Xadrez

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,35 @@
 package chess;
 
-public class ChessMatch {
+import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
+public class ChessMatch {
+	
+	private Board board; // Mesa de tabuleiro
+	
+	public ChessMatch() {
+		board = new Board(8,8); // Definindo o tamanho da mesa de xadrez
+		initialSetup();
+	}
+		
+	
+	
+	public ChessPiece[][] getPieces(){
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for(int i=0; i<board.getRows(); i++) {
+			for(int j=0; j<board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i,j); // Fez o downcast pra interpretar como peça de xadrez e n peça comum
+			}
+		}
+		return mat;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+	}
+	
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,12 +1,20 @@
 package chess.pieces;
 
-public class Rook {
-	private Integer turn;
-	private Color currentPlayer
-	private Boolean check;
-	private Boolean checkMate;
-	private ChessPiece enPassantVulnerable;
-	private ChessPiece promoted;
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece{
+	
+	public Rook(Board board, Color color) {
+		super(board, color);
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "R";
+	}
 	
 	
 	
